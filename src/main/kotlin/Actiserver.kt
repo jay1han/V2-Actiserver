c9d1e3d2533404abc6354fb318e5762ca7a9e51e
Jay Han <jayhan.name@gmail.com>
2024-05-21 21:58:48 +0200

    v415 Update stats before actimetre-query

--- src/main/kotlin/Actiserver.kt
+++ src/main/kotlin/Actiserver.kt
@@ -253,6 +253,9 @@ fun sideLoop() {
             return
         }
 
+        globalStat()
+        selfToCentral()
+
         val message = messageBuffer.array()
         val count = message[0].toInt()
         printLog("Query with $count items", 10)
